a63a4c00b2b14dab80ac714f1ab77c06d109428f
amanster22 <aman.shaik.us@gmail.com>
2023-11-06 16:27:42 -0500

    Add files via upload

---
+++ TamweelFinance/TamweelFinance/TamweelFinanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  TamweelFinanceApp.swift
+//  TamweelFinance
+//
+//  Created by Aman Shaik on 11/6/23.
+//
+
+import SwiftUI
+
+@main
+struct TamweelFinanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

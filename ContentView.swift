0b758248c9d37e16950efe88c0e43652a2b29ffb
amanster22 <aman.shaik.us@gmail.com>
2023-11-06 16:05:19 -0500

    Add files via upload

---
+++ ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IslamicFinanceApp
+//
+//  Created by Aman Shaik on 10/27/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Tamweel")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IslamicFinanceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IslamicFinanceAppApp.swift
+//  IslamicFinanceApp
+//
+//  Created by Aman Shaik on 10/27/23.
+//
+
+import SwiftUI
+
+@main
+struct IslamicFinanceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
